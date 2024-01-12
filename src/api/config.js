a2d5179c9b202a16d2b83e7000256729241e3907
eduardo <eduardo@Ubuntu.myguest.virtualbox.org>
2024-01-11 23:44:48 -0600

    Switch URL for API URL

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://127.0.0.1:3003';
+// const API_URL = 'http://127.0.0.1:3003';
 //const API_URL = 'http://math-app-server.herokuapp.com'; //'http://127.0.0.1:3003http://math-app-server.herokuapp.com';
+
+const API_URL = "https://mate-backend-app-kluo5.ondigitalocean.app"
 
 export { API_URL };
